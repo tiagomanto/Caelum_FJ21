bd0f8b1865911cf262bac160694768b8c7fff24a
Leandro Ferreira Fernandes <leandro.f.smart@gmail.com>
2015-08-29 18:13:57 -0300

    ...comitando metodo remove() do DAO

--- src/br/com/caelum/jdbc/dao/ContatoDAO.java
+++ src/br/com/caelum/jdbc/dao/ContatoDAO.java
@@ -106,4 +106,15 @@ public class ContatoDAO {
 		}
 	}
 
+	public void remove(Contato contato) {
+		try {
+			PreparedStatement stmt = connection.prepareStatement("delete from contatos where id=?");
+			stmt.setLong(1, contato.getId());
+			stmt.execute();
+			stmt.close();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
 }
